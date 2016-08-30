7558b4022494552a82e3740b7ac7f65dbaa3ead2
Vu Le <thenewvu@gmail.com>
2016-08-30 16:42:13 +0700

    WIP

--- src/index.js
+++ src/index.js
@@ -1,141 +1,188 @@
 'use strict';
 
-const _           = require('lodash');
-const dotpathWalk = require('dot-path-walk');
-const validator   = require('xana-validator-sync');
+const _         = require('lodash');
+const walkExtra = require('dot-path-walk');
+const validator = require('xana-validator-sync');
 
-const validatePluginOpts = validator.create([
-  ['commonFields', 'typeof', 'array', new TypeError('Expect pluginOpts.commonFields to be an array')],
-  ['secretFields', 'typeof', 'array', new TypeError('Expect pluginOpts.secretFields to be an array')],
-  ['cache', 'typeof', 'number', new TypeError('Expect pluginOpts.cache to be an number')],
-  ['limit', 'typeof', 'number', new TypeError('Expect pluginOpts.limit to be an number')],
-  ['modelName', 'required', true, new TypeError('Expect pluginOpts.modelName to be existed')],
-  ['modelName', 'typeof', 'string', new TypeError('Expect pluginOpts.modelName to be an string')]
+/**
+ * Default plugin options.
+ * @type {Object}
+ */
+const DEFAULT_PLUGIN_OPTS = {
+  commonFields: [],
+  secretFields: ['__v'],
+  readOnlyFields: [],
+  limit: 100,
+  lean: true
+};
+
+/**
+ * Plugin option validator.
+ * @type {Function}
+ */
+const pluginOptsValidator = validator.create([
+  // @formatter:off
+  ['commonFields', 'typeof'   , 'array'   , 'Expect commonFields to be an array'  ],
+  ['secretFields', 'typeof'   , 'array'   , 'Expect secretFields to be an array'  ],
+  ['cache'       , 'typeof'   , 'number'  , 'Expect cache to be an number'        ],
+  ['limit'       , 'typeof'   , 'number'  , 'Expect limit to be an number'        ],
+  ['lean'        , 'typeof'   , 'boolean' , 'Expect lean to be a boolean'         ],
+  ['modelName'   , 'required' , true      , 'Expect modelName to be existed'      ],
+  ['modelName'   , 'typeof'   , 'string'  , 'Expect modelName to be an string'    ]
+  // @formatter:on
 ]);
+
+/**
+ * Validate plugin options.
+ * @param opts - Plugin options.
+ */
+function validatePluginOpts(opts) {
+  const error = pluginOptsValidator(opts);
+  if (error) throw new TypeError(error.message);
+}
 
 
 /**
- *
+ * Plugin entry point.
  * @param schema
- * @param {Object} pluginOpts
- * @param {Array} [pluginOpts.commonFields]
- * @param {Array} [pluginOpts.secretFields]
- * @param {Number} [pluginOpts.cache]
- * @param {Number} [pluginOpts.limit]
- * @param {String} pluginOpts.modelName
+ * @param {Object}    pluginOpts
+ * @param {String}    pluginOpts.modelName
+ * @param {Array}    [pluginOpts.commonFields]
+ * @param {Array}    [pluginOpts.secretFields]
+ * @param {Array}    [pluginOpts.readOnlyFields]
+ * @param {Number}   [pluginOpts.cache]
+ * @param {Boolean}  [pluginOpts.lean]
+ * @param {Function} [pluginOpts.clearCache]
+ * @param {Number}   [pluginOpts.limit]
  */
 function plugin(schema, pluginOpts) {
-  pluginOpts = _.defaults({}, pluginOpts, {
-    commonFields: [],
-    secretFields: ['__v'],
-    limit: 100
-  });
+  pluginOpts = _.defaults({}, pluginOpts, DEFAULT_PLUGIN_OPTS);
+  validatePluginOpts(pluginOpts);
 
-  const validationError = validatePluginOpts(pluginOpts);
-  if (validationError) throw new TypeError(validationError.message);
+  const LIST_CACHE_PREFIX = `${pluginOpts.modelName}:list`;
+  const GET_CACHE_PREFIX  = `${pluginOpts.modelName}:get`;
 
   /**
-   *
+   * List documents.
    * @param query
    * @param extras
    * @param opts
-   * @param opts.limit
-   * @param opts.sort
-   * @param opts.lean
-   * @param opts.cache
    * @param done
    */
   schema.statics.list = function (query, extras, opts, done) {
-    extras = _.castArray(extras);
-    extras = [...pluginOpts.commonFields, ..._.without(extras, ...pluginOpts.secretFields)];
-
-    opts       = _.defaults({}, opts, {
-      limit: pluginOpts.limit,
-      lean: true,
-      cache: pluginOpts.cache
-    });
+    opts       = Object.assign({}, pluginOpts, opts);
     opts.limit = _.clamp(_.toSafeInteger(opts.limit), pluginOpts.limit);
-    opts.sort  = opts.sort && _.mapValues(opts.sort, (val) => val > 0 ? 1 : -1);
+
+    extras = _.castArray(extras);
+    extras = [...opts.commonFields, ..._.without(extras, ...opts.secretFields)];
 
     let find = this.find(query);
 
-    extras.forEach((extra) => {
-      for (const path of dotpathWalk(extra)) {
-        if (_.has(this.schema.path(path), 'options.ref')) {
+    for (const extra of extras) {
+      for (const path of walkExtra(extra)) {
+        const schema = this.schema.path(path);
+        if (_.has(schema, 'options.ref')) {
           find.populate(path);
           break;
         }
       }
-    });
+    }
 
-    find = find.select(extras.join(' ')).limit(opts.limit).sort(opts.sort);
+    find = find.select(extras.join(' '))
+               .limit(opts.limit)
+               .sort(opts.sort);
+
     opts.lean && find.lean();
-    const cachettl = typeof find.cache == 'function' ? (opts.cache || pluginOpts.cache) : 0;
-    cachettl && find.cache(cachettl, `${pluginOpts.modelName}:list:`);
+
+    const ttl = _.isFunction(find.cache) ? opts.cache : 0;
+    ttl && find.cache(ttl, LIST_CACHE_PREFIX);
+
     find.exec(done);
   };
 
   /**
-   *
+   * Get a document.
    * @param query
    * @param extras
    * @param opts
-   * @param opts.lean
-   * @param opts.cache
    * @param done
    */
   schema.statics.get = function (query, extras, opts, done) {
-    extras = _.castArray(extras);
-    extras = [...pluginOpts.commonFields, ..._.without(extras, ...pluginOpts.secretFields)];
+    opts = Object.assign({}, pluginOpts, opts);
 
-    opts = _.defaults({}, opts, {
-      lean: true,
-      cache: pluginOpts.cache
-    });
+    extras = _.castArray(extras);
+    extras = [...opts.commonFields, ..._.without(extras, ...opts.secretFields)];
 
     let find = this.findOne(query);
 
-    extras.forEach((extra) => {
-      for (const path of dotpathWalk(extra)) {
-        if (_.has(this.schema.path(path), 'options.ref')) {
+    for (const extra of extras) {
+      for (const path of walkExtra(extra)) {
+        const schema = this.schema.path(path);
+        if (_.has(schema, 'options.ref')) {
           find.populate(path);
           break;
         }
       }
-    });
+    }
 
     find = find.select(extras.join(' '));
+
     opts.lean && find.lean();
-    const cachettl = typeof find.cache == 'function' ? (opts.cache || pluginOpts.cache) : 0;
-    cachettl && find.cache(cachettl, `${pluginOpts.modelName}:list:`);
+
+    const ttl = _.isFunction(find.cache) ? opts.cache : 0;
+    ttl && find.cache(ttl, GET_CACHE_PREFIX);
+
     find.exec(done);
   };
 
+  /**
+   * Clear caches of getting the current document.
+   */
   schema.methods.clearCacheGet = function () {
     if (typeof pluginOpts.clearCache == 'function') {
-      pluginOpts.clearCache(`${pluginOpts.modelName}:get:*`);
+      pluginOpts.clearCache(`${GET_CACHE_PREFIX}*`);
     }
   };
 
+  /**
+   * Clear caches of listing.
+   *
+   */
   schema.methods.clearCacheList = function () {
     if (typeof pluginOpts.clearCache == 'function') {
-      pluginOpts.clearCache(`${pluginOpts.modelName}:list:*`);
+      pluginOpts.clearCache(`${LIST_CACHE_PREFIX}*`);
     }
   };
 
+  /**
+   * Patch the current document.
+   * @param patch
+   * @param done
+   */
   schema.methods.patch = function (patch, done) {
     const changes = Object.assign(this, _.omit(patch, pluginOpts.readOnlyFields));
     changes.save((err, doc) => done(err, doc));
   };
 
+  /**
+   * Pick common fields of the current document.
+   * @returns {*}
+   */
   schema.methods.pickCommonFields = function () {
     return _.pick(this.toObject(), pluginOpts.commonFields);
   };
 
+  /**
+   * Omit secret fields of the current document.
+   * @returns {*}
+   */
   schema.methods.omitSecretFields = function () {
     return _.omit(this.toObject(), pluginOpts.secretFields);
   };
 
+  /**
+   * Clear caches after patched a document.
+   */
   schema.post('save', function (doc) {
     doc.clearCacheGet();
     doc.clearCacheList();
